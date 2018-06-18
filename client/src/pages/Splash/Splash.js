6d0391e1574ff54ecde7991f0cdc4e41a4cb532d
Stetson Ramey <stetson.ramey@gmail.com>
2018-06-18 18:13:25 -0500

    edited the components section in the client folder, started writing the components.  Wrote the splash jumobotron component
    1. deleted all old remnant files from components
    2. added in Splash component

---
+++ client/src/pages/Splash/Splash.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import SplashHero from '../../components/Splash';
+
+const Splash = () => <SplashHero />;
+
+export default Splash;
