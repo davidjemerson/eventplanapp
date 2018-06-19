e763c7ae99dea5723a40aef641d48e13bdfc27c6
Stetson Ramey <stetson.ramey@gmail.com>
2018-06-18 20:02:03 -0500

    build the splash page body content
    1. added SplashBody component
    2. added component to splash page

---
+++ client/src/components/Splash/SplashBody.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export const SplashBody = props => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col mb-5">
+        <a href="https://placeholder.com">
+          <img src="http://via.placeholder.com/800x300" />
+        </a>
+      </div>
+      <div className="col mb-5">
+        <h1>ABOUT</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+          molestias soluta repellat suscipit reprehenderit modi ullam explicabo
+          vel illum neque ducimus minus, quod dolorem, maiores iure fugiat qui
+          sint! Accusantium incidunt sit alias repellat magnam tempora
+          laudantium nobis placeat doloribus, at quibusdam, optio natus earum.
+          Dolore repudiandae vero aliquam iste reprehenderit saepe aspernatur
+          nemo alias molestiae consectetur aliquid eligendi nostrum minima fugit
+          veritatis eos sapiente, rem et deserunt voluptas odit. Dolorum minus
+          consectetur, mollitia magnam deserunt odit porro explicabo
+          reprehenderit odio dignissimos natus molestiae quibusdam culpa ab aut
+          consequuntur ratione reiciendis sequi incidunt recusandae accusantium.
+          Dolore, natus at? Ullam, dolore!
+        </p>
+      </div>
+      <div className="w-100" />
+      <div className="col">
+        <h1 className="text-right">HOW IT WORKS</h1>
+        <p className="text-right">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+          iusto sit temporibus ratione quos totam dolor officiis aut! Sed,
+          officiis saepe fugiat voluptas similique beatae ea aut iste
+          accusantium, distinctio dolorem nobis ducimus quam a! Necessitatibus
+          alias a rem velit. Voluptatum, sunt. Doloribus nisi, consectetur
+          officia quibusdam, rem ea reprehenderit voluptatum non facere, maxime
+          repellendus possimus veniam mollitia quasi exercitationem a ipsam
+          dolores culpa accusantium iusto odio? Quibusdam ipsa voluptates
+          reprehenderit eius deleniti? Soluta sint culpa, amet architecto esse
+          nihil laboriosam quidem assumenda odio laudantium velit corporis,
+          veritatis quo atque. Qui et explicabo cumque libero, voluptates
+          incidunt sit perspiciatis itaque.
+        </p>
+      </div>
+      <div className="col">
+        <a href="https://placeholder.com">
+          <img src="http://via.placeholder.com/800x300" />
+        </a>
+      </div>
+    </div>
+  </div>
+);
