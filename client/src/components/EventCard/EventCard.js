0ef597485b8bfcda533438b4c57308e1ba9324b7
lschmittling <schmittling@gmail.com>
2018-06-20 14:05:32 -0500

    Added sidebar component, added to dashboard/home page

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -5,11 +5,11 @@ import "./EventCard.css";
 class EventCard extends Component {
   render() {
     return (
-      <div class="card text-white bg-success mb-3 event-card">
-        <div class="card-header">BOWLING</div>
-        <div class="card-body">
-          <h5 class="card-title">JUNE 29</h5>
-          <p class="card-text">2 of 7 Friends Going</p>
+      <div className="card text-white bg-success mb-3 event-card">
+        <div className="card-header">BOWLING</div>
+        <div className="card-body">
+          <h5 className="card-title">JUNE 29</h5>
+          <p className="card-text">2 of 7 Friends Going</p>
         </div>
       </div>
     );
