5bfcf8628a863f8956b4536ee5967af8a64d48a6
lschmittling <schmittling@gmail.com>
2018-06-20 13:34:51 -0500

    Finished EventCard added dashboard/home route in app.js

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import EventCard from '../../components/EventCard';
+import Footer from '../../components/Footer';
+
+const Home = () => (
+  <div>
+    <Navbar />
+    <EventCard />
+    <Footer />
+  </div>
+);
+
+export default Home;
