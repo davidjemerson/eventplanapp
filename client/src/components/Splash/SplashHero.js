48fea67115832d16d3f4ad236595708922de6746
lschmittling <schmittling@gmail.com>
2018-06-28 21:20:29 -0500

    Got rid of the input fields on homepage, added logout btn to navbar

--- client/src/components/Splash/SplashHero.js
+++ client/src/components/Splash/SplashHero.js
@@ -4,29 +4,16 @@ export const SplashHero = props => (
   <div className="jumbotron text-center jumbotron-fluid hero-img">
     <div className="container">
       <h1 className="display-4">PLAN YOUR EVENT TODAY</h1>
-      <p className="lead">
-        Plan, invite, vote and schedule your event today.
-      </p>
-      <form>
-        <div className="form-row align-items-center input-style">
-          <div className="col-auto">
-            <label className="sr-only" for="inlineFormInput">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control mb-2"
-              id="inlineFormInput"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div className="col-auto">
+      <p className="lead">Plan, invite, vote and schedule your event today.</p>
+      <div className="form-row align-items-center input-style">
+        <div className="col-auto">
+          <a href="/signin">
             <button type="submit" className="btn btn-outline mb-2 hero-btn">
               GET STARTED
             </button>
-          </div>
+          </a>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 );
