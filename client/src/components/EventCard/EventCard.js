41605d6ed123a05a87d7bcd8d21c773274778592
Stetson Ramey <stetson.ramey@gmail.com>
2018-06-30 11:49:18 -0500

    have cards populating with components
    1. dates are pulling in and formatting

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -7,8 +7,12 @@ const EventCard = props => (
   <div className="card text-white mb-3 event-card">
     <div className="card-header">{props.category}</div>
     <div className="card-body">
-      <h5 className="card-month">{props.date}</h5>
-      <h6 className="card-day">add the day as a prop</h6>
+      <h5 className="card-month">
+        <Moment format="MMMM">{props.date}</Moment>
+      </h5>
+      <h6 className="card-day">
+        <Moment format="D">{props.date}</Moment>
+      </h6>
       <p className="card-text">2 of 7 Friends Going</p>
     </div>
   </div>
