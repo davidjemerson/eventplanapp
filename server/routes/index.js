16d3dbf9475082ac2686c36fb49cb18989650714
djemerson <djemerson@gmail.com>
2018-07-09 20:30:25 -0500

    asdf

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ router.use("/auth", authRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build"));
 });
 
 module.exports = router;
